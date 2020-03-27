1e4256ba68fe02317c1bc888aeede8776663eded
Fernando Limón Flores <fernandolimonflores@Fernandos-MacBook-Pro.local>
2020-03-27 09:56:31 -0600

    tercero

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,13 @@
+initial(){
+  let input = document.querySelection(".fizzBuzzNumber");
+
+  let bt = document.querySelection("#submitBtn");
+
+  bt.addEventListener("click",(event)=>{
+    
+  })
+
+
+}
+
+initial()
